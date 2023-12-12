294e38f72aed2818734ae1e2e41f7fca61c514ec
Md. Rezaul Karim <96794203+devRezaulKarim@users.noreply.github.com>
2023-12-12 19:03:41 +0600

    Routing added

--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -1,10 +1,34 @@
-import { Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { ExpenseContext } from "../../Utilities/Context";
+import DetailsTable from "./DetailsTable";
 
 export default function Details() {
+  const { todaysExpenses, lastSevenDaysExpenses, thisMonthsExpenses } =
+    useContext(ExpenseContext);
+  const location = useLocation().pathname;
+  const expenses =
+    location === "/details/today"
+      ? todaysExpenses
+      : location === "/details/week"
+      ? lastSevenDaysExpenses
+      : thisMonthsExpenses;
+
+  console.log(expenses);
   return (
     <div>
       <div>Details</div>
-      <Outlet />
+      <div>
+        <Link to="/details/today">today</Link>
+        <Link to="/details/week">week</Link>
+        <Link to="/details/month">month</Link>
+      </div>
+
+      <div>
+        {expenses.map((expense, i) => (
+          <DetailsTable key={i} expense={expense} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/Utilities/Routes.jsx
+++ src/Utilities/Routes.jsx
@@ -1,9 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Details from "../Components/Details/Details";
 import App from "../App";
-import TodaysDetails from "../Components/Details/TodaysDetails";
-import WeeksDetails from "../Components/Details/WeeksDetails";
-import MonthsDetails from "../Components/Details/MonthsDetails";
 
 const Routes = createBrowserRouter([
   {
@@ -11,22 +8,8 @@ const Routes = createBrowserRouter([
     element: <App />,
   },
   {
-    path: "/details",
+    path: "/details/:dates",
     element: <Details />,
-    children: [
-      {
-        path: "/details/today",
-        element: <TodaysDetails />,
-      },
-      {
-        path: "/details/week",
-        element: <WeeksDetails />,
-      },
-      {
-        path: "/details/month",
-        element: <MonthsDetails />,
-      },
-    ],
   },
 ]);
 
